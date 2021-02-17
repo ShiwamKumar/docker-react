cf72947683ff659f79c61cc5e9820c00d733d265
shiwam <shiwam@cbnits.com>
2021-02-17 16:26:12 +0530

    added test final

--- src/App.test.js
+++ src/App.test.js
@@ -9,7 +9,7 @@ test('renders learn react link', () => {
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/l earn react/i);
   expect(linkElement).toBeInTheDocument();
 });
 
