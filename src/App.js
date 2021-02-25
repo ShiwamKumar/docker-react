5d607a9d1317ca140dcf056b812905e1c1f98f6a
shiwam <shiwam@cbnits.com>
2021-02-25 10:14:30 +0530

    added travis deploy config3.5

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Hellooooooooooo  There...!
+        Hellooooooooooo  There...! How are you?
         </p>
         <a
           className="App-link"
